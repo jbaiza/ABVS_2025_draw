e65472cb5ac1d7cbb4cde13754efc53c9ed7fff5
Jānis Baiža <jbaiza@gmail.com>
2025-06-02 14:30:52 +0300

    Add color change

--- draw.py
+++ draw.py
@@ -6,8 +6,10 @@ screen.bgcolor("black")
 pen = turtle.Turtle()
 pen.speed(0)
 pen.color("cyan")
+# colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
 for i in range(100):
+    # pen.color(colors[i % 6])
     pen.forward(i * 3)
     pen.right(45)
     pen.pensize(i / 20 + 1)
